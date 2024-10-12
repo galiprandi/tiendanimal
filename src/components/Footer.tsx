11660e2380bb80e8d42bdd264a97d2ef3e7d7ae1
Germán Aliprandi <german@egg.live>
2024-10-12 18:01:43 -0300

    ToggleLanguage

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,15 @@
+import styles from './Footer.module.css'
+import { ToggleLanguage } from './ToggleLanguage'
+
 export const Footer = () => {
-  return <footer>footer</footer>
+  return (
+    <footer className={styles.footer}>
+      <div>
+        <a href="https://galiprandi.github.io/me/" target="_blank">
+          <small>© Germán Aliprandi. 2023 All right reserved</small>
+        </a>
+      </div>
+      <ToggleLanguage />
+    </footer>
+  )
 }
---
+++ src/components/ToggleLanguage.tsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react'
+import i18n from '../libs/i18n'
+
+const availableLangs = {
+  en: '🇺🇸',
+  es: '🇪🇸',
+}
+
+export const ToggleLanguage = () => {
+  const [lang, setLang] = useState<AvailableLangs>(
+    i18n.language as AvailableLangs
+  )
+
+  const toggleLang = () => setLang(lang === 'en' ? 'es' : 'en')
+
+  useEffect(() => {
+    i18n.changeLanguage(lang)
+  }, [lang])
+
+  console.log({ i18n })
+
+  return (
+    <button className="icon" onClick={toggleLang}>
+      {availableLangs[lang]}
+    </button>
+  )
+}
+
+type AvailableLangs = keyof typeof availableLangs
