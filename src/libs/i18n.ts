7a4f8ec16a15146fd076b4d2a8e6402ef0d75708
Germán Aliprandi <german@egg.live>
2024-10-13 22:06:11 -0300

    Improve translations

--- src/libs/i18n.ts
+++ src/libs/i18n.ts
@@ -3,14 +3,20 @@ import { initReactI18next } from 'react-i18next'
 import Backend from 'i18next-http-backend'
 import LanguageDetector from 'i18next-browser-languagedetector'
 
+export const availableLangs = {
+  en: '🇺🇸',
+  es: '🇪🇸',
+}
+
 i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .use(Backend)
   .init({
     fallbackLng: 'en',
+    supportedLngs: ['en', 'es'],
     backend: {
-      loadPath: 'locales/{{lng}}/{{ns}}.json',
+      loadPath: '/tiendanimal/locales/{{lng}}/{{ns}}.json',
     },
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
